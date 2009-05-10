80ca317e96fbf8940ffdd13b4a7f6e94770db8b8
djpnewton <djpnewton@gmail.com>
2009-05-10 03:18:50 +0000

    * converted python hsm implementation to use classes

--- simplehsm_python/simplehsm.py
+++ simplehsm_python/simplehsm.py
@@ -9,67 +9,69 @@ SIG_EXIT  = 3
 SIG_USER  = 4
 
 #
-# Current state variable
+# SimpleHsm class
 #
 
-__cur_state = None
+class SimpleHsm:
 
-#
-# State utility function implementations
-#
+    #
+    # Current state variable
+    #
 
-def InitialState(new_state):
-    InitTransitionState(new_state)
+    __cur_state = None
 
-def __IsParent(parent_state, child_state):
-    while True:
-        child_state = child_state(SIG_NULL, None)
-        if child_state == parent_state:
-            return True
-        if not child_state: break
-    return False
+    #
+    # State utility function implementations
+    #
 
-def TransitionState(new_state):
-    # exit signal to current state
-    global __cur_state
-    if (__cur_state):
-        __cur_state(SIG_EXIT, None)
-        parent_state = __cur_state(SIG_NULL, None)
-        while (not __IsParent(parent_state, new_state)):
-            parent_state(SIG_EXIT, None)
-            parent_state = parent_state(SIG_NULL, None)
-    else:
-        print "TransitionState: ERROR - current state is invalid!"
-    InitTransitionState(new_state)
+    def InitialState(self, new_state):
+        self.InitTransitionState(new_state)
 
-def InitTransitionState(new_state):
-    # set new state
-    global __cur_state
-    __cur_state = new_state
-    # entry signal to current state
-    SignalCurrentState(SIG_ENTRY, None)
-    # init signal to current state
-    SignalCurrentState(SIG_INIT, None)
+    def __IsParent(self, parent_state, child_state):
+        while True:
+            child_state = child_state(SIG_NULL, None)
+            if child_state == parent_state:
+                return True
+            if not child_state: break
+        return False
 
-def SignalCurrentState(signal, param):
-    global __cur_state
-    if (__cur_state != None):
-        parent_state = __cur_state
+    def TransitionState(self, new_state):
+        # exit signal to current state
+        if (self.__cur_state):
+            self.__cur_state(SIG_EXIT, None)
+            parent_state = self.__cur_state(SIG_NULL, None)
+            while (not self.__IsParent(parent_state, new_state)):
+                parent_state(SIG_EXIT, None)
+                parent_state = parent_state(SIG_NULL, None)
+        else:
+            print "TransitionState: ERROR - current state is invalid!"
+        self.InitTransitionState(new_state)
+
+    def InitTransitionState(self, new_state):
+        # set new state
+        self.__cur_state = new_state
+        # entry signal to current state
+        self.SignalCurrentState(SIG_ENTRY, None)
+        # init signal to current state
+        self.SignalCurrentState(SIG_INIT, None)
+
+    def SignalCurrentState(self, signal, param):
+        if (self.__cur_state != None):
+            parent_state = self.__cur_state
+            while (True):
+                parent_state = parent_state(signal, param)
+                if (not parent_state): break;
+        else:
+            print "SignalCurrentState: ERROR - current state is invalid!\n"
+
+    def IsInState(self, state):
+        parent_state = self.__cur_state
         while (True):
-            parent_state = parent_state(signal, param)
-            if (not parent_state): break;
-    else:
-        print "SignalCurrentState: ERROR - current state is invalid!\n"
-
-def IsInState(state):
-    global __cur_state
-    parent_state = __cur_state
-    while (True):
-        if (state == parent_state):
-            return True
-        parent_state = parent_state(SIG_NULL, None)
-        if (not parent_state): break
-    return False
+            if (state == parent_state):
+                return True
+            parent_state = parent_state(SIG_NULL, None)
+            if (not parent_state): break
+        return False
 
 
 
