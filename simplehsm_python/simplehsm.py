a8514ea1d8cccbda8e188d5e0ab1b8f6c3834935
djpnewton <djpnewton@gmail.com>
2009-05-17 04:43:05 +0000

    * changed python implementation to use a StateEvent class for the state function parameters

--- simplehsm_python/simplehsm.py
+++ simplehsm_python/simplehsm.py
@@ -9,6 +9,17 @@ SIG_EXIT  = 3
 SIG_USER  = 4
 
 #
+# State Event class
+#
+
+class StateEvent:
+
+    sig = None
+
+    def __init__(self, sig):
+        self.sig = sig
+
+#
 # SimpleHsm class
 #
 
@@ -29,7 +40,7 @@ class SimpleHsm:
 
     def __IsParent(self, parent_state, child_state):
         while True:
-            child_state = child_state(SIG_NULL, None)
+            child_state = child_state(StateEvent(SIG_NULL))
             if child_state == parent_state:
                 return True
             if not child_state: break
@@ -38,11 +49,11 @@ class SimpleHsm:
     def TransitionState(self, new_state):
         # exit signal to current state
         if (self.__cur_state):
-            self.__cur_state(SIG_EXIT, None)
-            parent_state = self.__cur_state(SIG_NULL, None)
+            self.__cur_state(StateEvent(SIG_EXIT))
+            parent_state = self.__cur_state(StateEvent(SIG_NULL))
             while (not self.__IsParent(parent_state, new_state)):
-                parent_state(SIG_EXIT, None)
-                parent_state = parent_state(SIG_NULL, None)
+                parent_state(StateEvent(SIG_EXIT))
+                parent_state = parent_state(StateEvent(SIG_NULL))
             # set current state to parent state
             self.__cur_state = parent_state
         else:
@@ -53,33 +64,33 @@ class SimpleHsm:
             parent_state = new_state
             last_child = new_state
             while (True):
-                parent_state = parent_state(SIG_NULL, None)
+                parent_state = parent_state(StateEvent(SIG_NULL))
                 if (parent_state == self.__cur_state):
-                    last_child(SIG_ENTRY, None)
+                    last_child(StateEvent(SIG_ENTRY))
                     # set current state to last child state
                     self.__cur_state = last_child
                     break;
                 last_child = parent_state
 
         # init signal to new state
-        new_state(SIG_INIT, None)
+        new_state(StateEvent(SIG_INIT))
 
     def InitTransitionState(self, new_state):
         # set new state
         self.__cur_state = new_state
         if (self.__cur_state):
             # entry signal to current state
-            self.__cur_state(SIG_ENTRY, None)
+            self.__cur_state(StateEvent(SIG_ENTRY))
             # init signal to current state
-            self.__cur_state(SIG_INIT, None)
+            self.__cur_state(StateEvent(SIG_INIT))
         else:
             print "InitTransitionState: ERROR - current state is invalid!\n"
 
-    def SignalCurrentState(self, signal, param):
+    def SignalCurrentState(self, state_event):
         if (self.__cur_state != None):
             parent_state = self.__cur_state
             while (True):
-                parent_state = parent_state(signal, param)
+                parent_state = parent_state(state_event)
                 if (not parent_state): break;
         else:
             print "SignalCurrentState: ERROR - current state is invalid!\n"
@@ -89,7 +100,7 @@ class SimpleHsm:
         while (True):
             if (state == parent_state):
                 return True
-            parent_state = parent_state(SIG_NULL, None)
+            parent_state = parent_state(StateEvent(SIG_NULL))
             if (not parent_state): break
         return False
 
