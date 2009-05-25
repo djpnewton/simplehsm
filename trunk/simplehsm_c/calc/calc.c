c0883a7db63cbbbcaa3e2a59b32fbbd70ea27a0d
djpnewton <djpnewton@fdb09e2a-3b90-11de-b5fc-91815ed80701>
2009-05-25 06:31:27 +0000

    * hooked up button click callbacks

--- trunk/simplehsm_c/calc/calc.c
+++ trunk/simplehsm_c/calc/calc.c
@@ -6,12 +6,37 @@
 
 #include "simplehsm.h"
 
+int btnClick(Ihandle* self)
+{
+  printf("self: %d\n", self);
+  return EXIT_SUCCESS;
+}
+
+void AttachButtonEvents(Ihandle* ih)
+{
+  int i;
+  // recurse through all controls
+  for (i = 0; i < IupGetChildCount(ih); i++)
+    AttachButtonEvents(IupGetChild(ih, i));
+  // if control is a button attach the event
+  if (strcmp(IupGetClassName(ih), "button") == 0)
+    IupSetCallback(ih, "ACTION", btnClick);
+}
+
 int main(int argc, char **argv)
 {
+  // initialise iup library
   IupOpen(&argc, &argv);
+  // load calc user interface file
   IupLoad("calc.led");
+  // setup button handlers 
+  AttachButtonEvents(IupGetHandle("main"));
+  // show main window
   IupShow(IupGetHandle("main"));
+  // start mainloop
   IupMainLoop();
+  // close iup library
   IupClose();  
+
   return EXIT_SUCCESS;
 }
